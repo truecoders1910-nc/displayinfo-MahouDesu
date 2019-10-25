08b19686f26b6e86eceb847534f4b93bbc381d11
Dylan Masters <Dy1anmasters93@aol.com>
2019-10-24 20:23:42 -0500

    Created variables, and a series of WriteLines and ReadLines to take user input and display it to the user.

--- DisplayInfo/Program.cs
+++ DisplayInfo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace DisplayInfo
 {
@@ -6,15 +7,51 @@ namespace DisplayInfo
     {
         static void Main(string[] args)
         {
+            string firstName;
+            string lastName;
+            string favoriteNumber;
+            int favNumberValue;
+            string favoriteAnimal;
+
             // Get first and last name from user input
+            Console.WriteLine("Please input your First Name");
+            firstName = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Please input your Last Name");
+            lastName = Console.ReadLine();
+            Console.Clear();
+
             // Get their favorite number
+            Console.WriteLine("Please input your Favorite Number");
+            favoriteNumber = Console.ReadLine();
+            favNumberValue = Convert.ToInt32(favoriteNumber);
+            Console.Clear();
             // Get their favorite animal
+            Console.WriteLine("Please input your Favorite Animal");
+            favoriteAnimal = Console.ReadLine();
+            Console.Clear();
 
-            // Once you have gotten all their info and stored it,
-            // display it back to them.
-
+            // Once you have gotten all their info and stored it, display it back to them.
             // If their favorite number is greater than 10, display it in green.
             // If their favorite number is less than 5, display it in red.
+            if (favNumberValue > 10)
+            {
+                Console.WriteLine($"From the provided data you have sent me your name is {firstName} {lastName}.");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Your favorite number is {favoriteNumber}.");
+                Console.ResetColor();
+                Console.WriteLine($"and your favorite animal is a {favoriteAnimal}.");
+            }
+            else if (favNumberValue < 5)
+            {
+                Console.WriteLine($"From the provided data you have sent me your name is {firstName} {lastName}.");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Your favorite number is {favoriteNumber}.");
+                Console.ResetColor();
+                Console.WriteLine($"and your favorite animal is a {favoriteAnimal}.");
+            }
+            
         }
     }
 }
